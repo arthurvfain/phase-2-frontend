234e7214e02ecb3a7b7f90d0fc3d09580b8eaf97
Arthur V Fain <arthurvfain@gmail.com>
2021-06-21 14:40:32 -0400

    what a mess we made

--- flatiron-coliseum/src/Components/PokeCard.js
+++ flatiron-coliseum/src/Components/PokeCard.js
@@ -3,30 +3,40 @@ import {Card} from '@material-ui/core'
 function PokeCard({name, url}) {
 
     
+    function getPokemon()
+    {
+        let pokemon;
+        fetch(url).then(res => res.json()).then(data => pokemon = data)
+        return pokemon;
+    }
     
-    let frontImageURL
-    let backImageURL
-    fetch(url).then(res => res.json()).then(data => {
+    let pokeObj = getPokemon()
+    //{name: pokemon.name, frontUrl: pokemon.sprites.front_default, backUrl: pokemon.sprites.back_default}
+    
+    // let frontImageURL
+    // let backImageURL
+    // fetch(url).then(res => res.json()).then(data => {
    
-       console.log(data.sprites.front_default)
-        frontImageURL = data.sprites.front_default;
-        backImageURL = data.sprites.back_default;
-    })
+    //    console.log(data.sprites.front_default)
+    //     frontImageURL = data.sprites.front_default;
+    //     backImageURL = data.sprites.back_default;
+    // })
     
     // function frontImage(){
     //     return frontImageURL}
+    console.log(pokeObj)
 
-while (frontImageURL === ''){
-    return <p>Loading...</p>
-}
-{
+// while (frontImageURL === ''){
+//     return <p>Loading...</p>
+// }
+// {
     return (
         <Card>
         <p>{name}</p>
-        <img src={frontImageURL} />
+        {/* <img src={pokeObj.frontUrl} /> */}
         </Card>
     )
-}
+// }
 }
 
 export default PokeCard;
