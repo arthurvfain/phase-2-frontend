61ae3ca5df1351830b01e604649ed43739fe7324
Arthur V Fain <arthurvfain@gmail.com>
2021-06-25 13:42:53 -0400

    filter button options

--- flatiron-coliseum/src/Components/Filter.js
+++ flatiron-coliseum/src/Components/Filter.js
@@ -1,10 +1,27 @@
-import TextField from '@material-ui/core/TextField';
+import {TextField, Avatar, InputAdornment } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+    },
+  }));
 
 function Filter({filterInput, handleSearch}) {
 
+    const classes = useStyles();
+   
     return (
         <form>
-            <TextField style={{backgroundColor: 'white'}} id="outlined-basic" label="PokéSearch"  value={filterInput} onChange={handleSearch} />
+            {/* <TextField style={{backgroundColor: 'white', borderRadius: '5px'}} id="outlined-basic" label="PokéSearch" InputProps={{disableUnderline: true, }}  value={filterInput} onChange={handleSearch} /> */}
+            <TextField style={{backgroundColor: 'white', borderRadius: '5px'}} label="PokéSearch" value={filterInput} onChange={handleSearch} InputProps={{disableUnderline: true, startAdornment: (<InputAdornment position="start"><Avatar className={classes.small} alt="poke" src="https://jenessa-reika.ca/wp-content/uploads/2020/04/pokemon2.png" /></InputAdornment>),}}/>
         </form>
     )
 }
--- flatiron-coliseum/src/Components/Home.js
+++ flatiron-coliseum/src/Components/Home.js
@@ -4,7 +4,7 @@ function Home()
     return (
         <div>
             <br/>
-            <iframe autoPlay="1" width="560" height="315" src="https://www.youtube.com/embed/rg6CiPI6h2g" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe autoPlay={true} width="560" height="315" src="https://www.youtube.com/embed/rg6CiPI6h2g" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     )
 }
